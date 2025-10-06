0f6f0eba5bb0da3b5ba3f22e7526bf7bb1fd1bed
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-10-06 17:35:54 +0300

    Add project files.

---
+++ Blogy.Entity/Entities/Tag.cs
@@ -0,0 +1,11 @@
+﻿using Blogy.Entity.Entities.Common;
+
+namespace Blogy.Entity.Entities
+{
+    public class Tag : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public IList<BlogTag> BlogTags { get; set; }
+    }
+}
