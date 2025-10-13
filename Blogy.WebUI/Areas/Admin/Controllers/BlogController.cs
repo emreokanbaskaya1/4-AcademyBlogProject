b972987b63518e1f5c307bda66d38bad91a94758
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-10-14 00:53:42 +0300

    Add category support and blog creation functionality
    Refactored `BlogController` to include category support by
    injecting `ICategoryService` and adding a `GetCategoriesAsync`
    method to populate dropdowns. Introduced `CreateBlog` GET and
    POST actions to enable blog creation with category selection.
    Updated `using` directives to include necessary DTOs and
    services. Moved `[Area("Admin")]` attribute for better
    organization.

--- Blogy.WebUI/Areas/Admin/Controllers/BlogController.cs
+++ Blogy.WebUI/Areas/Admin/Controllers/BlogController.cs
@@ -1,15 +1,47 @@
-﻿using Blogy.Business.Services.BlogServices;
+﻿using Blogy.Business.DTOs.BlogDtos;
+using Blogy.Business.Services.BlogServices;
+using Blogy.Business.Services.CategoryServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Blogy.WebUI.Areas.Admin.Controllers
 {
-    public class BlogController(IBlogService _blogService) : Controller
+    [Area("Admin")]
+    public class BlogController(IBlogService _blogService, ICategoryService _categoryService) : Controller
     {
-        [Area("Admin")]
+        private async Task GetCategoriesAsync()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            ViewBag.Categories = (from category in categories
+                                  select new SelectListItem
+                                  {
+                                      Text = category.CategoryName,
+                                      Value = category.Id.ToString()
+                                  }).ToList();
+        }
         public async Task<IActionResult> Index()
         {
             var blogs = await _blogService.GetBlogWithCategoriesAsync();
             return View(blogs);
+        }
+
+        public async Task<IActionResult> CreateBlog()
+        {
+            await GetCategoriesAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBlog(CreateBlogDto blogDto)
+        {
+            if (!ModelState.IsValid) 
+            {
+                await GetCategoriesAsync();
+                return View(blogDto);
+            }
+
+            await _blogService.CreateAsync(blogDto);
+            return RedirectToAction("Index");
         }
     }
 }
