9fb987f94e78607670d2e0230e59fbc0a60a2016
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-11-04 13:18:27 +0300

    Add user profile management functionality
    Introduced `EditProfileDto` for handling user profile data,
    including properties for personal details, profile picture,
    and password. Added `UserMappings` class for AutoMapper
    configurations to map `AppUser` to `EditProfileDto` and
    `ResultUserDto`.
    
    Created `ProfileController` to manage profile-related
    operations, restricted to Admin users. Implemented an
    `Index` action to retrieve and display user profile data.
    
    Added a Razor view (`Index.cshtml`) for editing profile
    details, including a form for updating user information
    and uploading a profile picture. Integrated validation
    and file upload handling.

--- Blogy.Business/Mappings/UserMappings.cs
+++ Blogy.Business/Mappings/UserMappings.cs
@@ -1,15 +1,21 @@
 ﻿using AutoMapper;
 using Blogy.Business.DTOs.UserDtos;
 using Blogy.Entity.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace Blogy.Business.Mappings
 {
+    
     public class UserMappings: Profile
     {
         public UserMappings() {
             // Use string concatenation instead of string.Join to avoid params/ReadOnlySpan issues in expression trees
             CreateMap<AppUser, ResultUserDto>()
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FirstName + " " + src.LastName));
+
+            CreateMap<AppUser, EditProfileDto>().ReverseMap();
         }
     }
 }
---
+++ Blogy.WebUI/Areas/Admin/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Blogy.Business.DTOs.UserDtos;
+using Blogy.Entity.Entities;
+using Blogy.WebUI.Consts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = $"{Roles.Admin}")]
+    public class ProfileController(UserManager<AppUser> _userManager, IMapper _mapper) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var editUser = _mapper.Map<EditProfileDto>(user);
+
+            return View(editUser);
+        }
+    }
+}
