ee2fa8d509dfe82b63d63bc1a7c334bb3875cf8b
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-11-05 16:20:21 +0300

    Add profile editing and password management features
    Updated `EditProfileDto` to allow nullable properties for `Title`, `ImageUrl`, and `ImageFile`. Enhanced `ProfileController` to handle image uploads and profile updates, including password validation and user property updates.
    
    Added `ChangePasswordController` and `ChangePasswordDto` to enable password change functionality. Created a new `Index.cshtml` view for the password change form. Updated `Blogy.WebUI.csproj` to include a folder for storing uploaded images.
    
    Added two new image files (`01965947-1278-4255-93ef-b1daecdcad3c.png` and `dc19555d-834a-4c28-8476-cd29e0772f8a.png`) to the project.

--- Blogy.Business/DTOs/UserDtos/EditProfileDto.cs
+++ Blogy.Business/DTOs/UserDtos/EditProfileDto.cs
@@ -7,9 +7,9 @@ namespace Blogy.Business.DTOs.UserDtos
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
-        public string Title { get; set; }
-        public string ImageUrl { get; set; }
-        public IFormFile ImageFile { get; set; }
+        public string? Title { get; set; }
+        public string? ImageUrl { get; set; }
+        public IFormFile? ImageFile { get; set; }
         public string CurrentPassword { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
--- Blogy.WebUI/Areas/Admin/Controllers/ProfileController.cs
+++ Blogy.WebUI/Areas/Admin/Controllers/ProfileController.cs
@@ -10,7 +10,7 @@ namespace Blogy.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize(Roles = $"{Roles.Admin}")]
-    public class ProfileController(UserManager<AppUser> _userManager, IMapper _mapper) : Controller
+    public class ProfileController(UserManager<AppUser> _userManager, IMapper _mapper, IWebHostEnvironment _webHostEnvironment) : Controller
     {
         public async Task<IActionResult> Index()
         {
@@ -20,5 +20,57 @@ namespace Blogy.WebUI.Areas.Admin.Controllers
 
             return View(editUser);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(EditProfileDto model)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var passwordCorrect = await _userManager.CheckPasswordAsync(user, model.CurrentPassword);
+
+            if (!passwordCorrect)
+            {
+                ModelState.AddModelError("", "The current password that was entered is not correct");
+                return View(model);
+            }
+
+            if (model.ImageFile is not null)
+            {
+                var extension = Path.GetExtension(model.ImageFile.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLocation = Path.Combine(_webHostEnvironment.WebRootPath, "images", imageName);
+                
+                var imageDirectory = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                if (!Directory.Exists(imageDirectory))
+                {
+                    Directory.CreateDirectory(imageDirectory);
+                }
+                
+                using var stream = new FileStream(saveLocation, FileMode.Create);
+                await model.ImageFile.CopyToAsync(stream);
+                user.ImageUrl = "/images/" + imageName;
+            }
+
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.Email = model.Email;
+            user.PhoneNumber = model.PhoneNumber;
+            user.UserName = model.UserName;
+            user.Title = model.Title;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Update error");
+                return View(model);
+
+            }
+
+            return RedirectToAction("Index", "Blog", new {area=Roles.Admin});
+
+
+
+        }
     }
+
+
 }
